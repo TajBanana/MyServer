c1f6fbc214c162f21777d083508ca6d62ba01bcf
TajBanana <ytahjian@gmail.com>
2021-12-14 12:37:32 +0800

    starting files

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    private ServerSocket httpServer;
+    private Socket serverSocket;
+    private int port;
+
+
+    public boolean checkDir() {
+        return false;
+    }
+
+    public void openPort(int port) throws IOException {
+        ServerSocket myServer = new ServerSocket(3000);
+        System.out.println("[SERVER] Waiting for client connection...");
+        Socket serverSocket = myServer.accept();
+        System.out.println("[SERVER] Connection established");
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        if --port arg[0], --docroot rag[1]
+
+        int port = 3000;
+        String docRoot = "/static";
+        List<String> argsArray = new ArrayList<>();
+
+
+        if (args != null && args.length>=1) {
+            argsArray.addAll(Arrays.asList(args));
+
+            if (!(argsArray.contains("--port")) && !(argsArray.contains("--docRoot"))) {
+//            System.out.println(argsArray);
+                System.out.println("please input correct commands");
+                return;
+            }
+        }
+
+        if ((argsArray.contains("--port")) && (argsArray.contains("--docRoot"))) {
+            String portString = argsArray.get(argsArray.indexOf("--port") + 1);
+            port = Integer.parseInt(portString);
+
+            String docRootInput = argsArray.get(argsArray.indexOf("--docRoot") + 1);
+            docRoot = docRootInput.substring(docRootInput.indexOf(":")+1);
+
+        } else if ((argsArray.contains("--port"))) {
+            String portString = argsArray.get(argsArray.indexOf("--port") + 1);
+            port = Integer.parseInt(portString);
+
+        } else if ((argsArray.contains("--docRoot"))) {
+            String docRootInput = argsArray.get(argsArray.indexOf("--docRoot") + 1);
+            docRoot = docRootInput.substring(docRootInput.indexOf(":")+1);
+        }
+
+
+        System.out.println(docRoot);
+        System.out.println(port);
+
+//  ----------------------------------------------------------------------------------------    OPEN SERVER & PORT
+        HttpServer httpServer = new HttpServer();
+        httpServer.openPort(port);
+
+
+
+ /*       ServerSocket server = new ServerSocket(3000);
+        System.out.println("[SERVER] Waiting for client connection...");
+        Socket serverSocket = server.accept();
+        System.out.println("[SERVER] Connection established");*/
+
+
+
+    }
+}
